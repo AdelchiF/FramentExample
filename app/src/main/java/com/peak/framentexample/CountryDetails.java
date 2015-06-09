b60f85a793fd8ec3b570348d577ab5944cea1a7e
AdelchiF <adelchif@gmail.com>
2015-06-09 11:45:55 +0200

    Primo commit dell'esempio di utilizzo dei fragment

---
+++ app/src/main/java/com/peak/framentexample/CountryDetails.java
@@ -0,0 +1,87 @@
+package com.peak.framentexample;
+
+import android.app.Activity;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.peak.framentexample.dummy.DummyContent;
+
+
+public class CountryDetails extends Fragment {
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    private TextView textViewCountryTitle;
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment CityFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static CountryDetails newInstance(String param1, String param2) {
+        CountryDetails fragment = new CountryDetails();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    public CountryDetails() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        Bundle bundle = getArguments();
+        if(bundle != null){
+            String countryId = bundle.getString("country");
+            onCountrySelected(countryId);
+        }
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_details, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        textViewCountryTitle = (TextView)view.findViewById(R.id.countrytitle);
+    }
+
+    public void onCountrySelected(String countryId){
+        String country = DummyContent.ITEMS.get(Integer.parseInt(countryId)-1).content;
+        textViewCountryTitle.setText(country);
+    }
+
+}
---
+++ app/src/main/java/com/peak/framentexample/MainActivity.java
@@ -0,0 +1,51 @@
+package com.peak.framentexample;
+
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class MainActivity extends AppCompatActivity implements CountryFragment.OnFragmentInteractionListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        if (findViewById(R.id.container) != null)
+        {
+            // Se è presente il FrameLayout con id container,
+            // vuol dire che siamo in SINGLE-PANE perciò
+            // è necessario aggiungere il Fragment con la transazione.
+
+            // Se savedInstanceState non è nullo, non siamo alla
+            // prima visualizzazione perciò non serve aggiungere il Fragment.
+
+            if (savedInstanceState != null)
+                return;
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.container, CountryFragment.newInstance("a", "b")).commit();
+        }
+    }
+
+    @Override
+    public void onFragmentInteraction(String id) {
+        CountryDetails countryDetails = (CountryDetails)getSupportFragmentManager().findFragmentById(R.id.cityfrag);
+
+        if(countryDetails != null && countryDetails.isInLayout()){
+            countryDetails.onCountrySelected(id);
+        }else {
+            CountryDetails fragCountryDetails = new CountryDetails();
+            Bundle b = new Bundle();
+            b.putString("country", id);
+            fragCountryDetails.setArguments(b);
+            FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+            ft.replace(R.id.container, fragCountryDetails);
+            ft.addToBackStack(null);
+            ft.commit();
+        }
+    }
+}
